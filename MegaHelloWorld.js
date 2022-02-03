20e3415b791d93dab65303b63019593661e4b04b
JulianMorales1 <98353349+JulianMorales1@users.noreply.github.com>
2022-02-03 15:22:02 -0500

    Create MegaHelloWorld.js

---
+++ MegaHelloWorld.js
@@ -0,0 +1,7 @@
+let first_line = console.log("HH   HH        lll lll                                  lll      dd !!!"); 
+let second_line = console.log("HH   HH   eee  lll lll  oooo   ww      ww  oooo  rr rr  lll      dd !!!"); 
+let third_line = console.log("HHHHHHH ee   e lll lll oo  oo  ww      ww oo  oo rrr  r lll  dddddd !!!");
+let fourth_line = console.log("HH   HH eeeee  lll lll oo  oo   ww ww ww  oo  oo rr     lll dd   dd !!!"); 
+let fifth_line = console.log("HH   HH  eeeee lll lll  oooo     ww  ww    oooo  rr     lll  dddddd !!!");
+
+console.log(first_line + "\n" + second_line + "\n" + third_line + "\n" + fourth_line + "\n" + fifth_line)
